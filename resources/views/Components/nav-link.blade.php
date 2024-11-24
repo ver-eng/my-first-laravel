b86ddff54642245996af9575301ad2aeae64188e
ver-eng <v.patsinashvili@gmail.com>
2024-11-24 16:57:17 +0400

    feat: routes

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,5 +1,3 @@
 @props(['active'=>false])
 
-
-
 <a  class="{{$active? 'text-white bg-gray-900 ':'text-gray-300 hover:bg-gray-700 hover:text-white'}}rounded-md  px-3 py-2 text-sm font-medium " aria-current="{{$active? 'page':'false'}}" {{$attributes}}>{{$slot}}</a>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
   <x-slot:heading>Home page
 </x-slot:heading>
-<h1>Hello from the Home page</h1>
+
 </x-layout>
